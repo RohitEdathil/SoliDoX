07947be80034b3b4701f9dffc7738734d8444516
Rohit V <rohitedathil@gmail.com>
2023-04-01 23:12:15 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+# SoliDoX
+
+A blockchain-based platform for the creation and verification of digital documents.
+
+Note: This is a work in progress. The code is not yet ready for production use.
---
+++ solidox-contract/contracts/SoliDoX.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+/* 
+
+SoliDoX is a contract for non-fungible documents.
+Each document is represented by a unique ID.
+
+Format of the data:
+- With URI: SHA256_HASH-FILE_URL-EXPIRATION_TIMESTAMP
+- Without URI: SHA256_HASH-EXPIRATION_TIMESTAMP
+
+*/
+contract SoliDoX is ERC721URIStorage {
+    // Generate unique token IDs
+    using Counters for Counters.Counter;
+    Counters.Counter private _documentIdCounter;
+
+    constructor() ERC721("SoliDoX", "SDX") {}
+
+    function issueDocument(string memory data) public returns (uint256) {
+        // Fetches the current value
+        uint256 documentId = _documentIdCounter.current();
+
+        // Issues the new document
+        _mint(msg.sender, documentId);
+        _setTokenURI(documentId, data);
+
+        // Increments the counter by two
+        _documentIdCounter.increment();
+        _documentIdCounter.increment();
+
+        return documentId;
+    }
+
+    function revokeDocument(uint256 documentId) public returns (uint256) {
+        // Checks if the document exists
+        require(_exists(documentId), "Document does not exist");
+
+        // Checks if the document is owned by the caller
+        require(ownerOf(documentId) == msg.sender, "Caller is not the issuer");
+
+        // Revokes the document
+        _burn(documentId);
+
+        return documentId;
+    }
+
+    function getDocumentData(
+        uint256 documentId
+    ) public view returns (string memory) {
+        // Checks if the document exists
+        require(_exists(documentId), "Document does not exist");
+
+        // Returns the document data
+        return tokenURI(documentId);
+    }
+}
