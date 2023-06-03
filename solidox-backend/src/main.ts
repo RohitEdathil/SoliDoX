043395a9257e456ac7032aa8e11f166a99b1510c
Rohit V <rohitedathil@gmail.com>
2023-06-03 17:18:07 +0530

    CORS enabled

--- solidox-backend/src/main.ts
+++ solidox-backend/src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // CORS
+  app.enableCors();
+
   // Pipes
   app.useGlobalPipes(new ValidationPipe());
 
