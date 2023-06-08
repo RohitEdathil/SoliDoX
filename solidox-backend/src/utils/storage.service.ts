1f8392f9834eb1fff50baf9fd0f52840a6f13d57
Rohit V <rohitedathil@gmail.com>
2023-06-09 00:12:41 +0530

    Extra } bug fix

--- solidox-backend/src/utils/storage.service.ts
+++ solidox-backend/src/utils/storage.service.ts
@@ -6,7 +6,7 @@ export class StorageService {
   constructor() {}
 
   async save(file: Express.Multer.File, path: string) {
-    const location = `storage/${path}}`;
+    const location = `storage/${path}`;
 
     // Store to local file system
     await outputFile(location, file.buffer);
